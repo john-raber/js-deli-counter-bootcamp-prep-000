7bc11b57c256394873708623ab06db01e87ef489
John Raber <jrab224@gmail.com>
2018-04-15 20:03:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,5 @@ var katzDeliLine= [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  if (katzDeliLine.length === 1) {
-      return `Welcome, ${name}. You are number ${katzDeliLine.indexof(name) + 1} in line.`;
-  } else {
-      return `
-  }
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexof(name) + 1} in line.`;
 }
