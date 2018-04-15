ed3fd0a49c608ba4be49a06c95f7cfe1cfae807e
John Raber <jrab224@gmail.com>
2018-04-15 20:12:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing() {
-  if (katzDeliLine.length === 0) {
-      return "There is nobody waiting to be served!";
-  } else {
+  if (katzDeliLine.length > 0) {
       return `Currently serving ${katzDeliLine.shift()}.`;
+  } else {
+      return "There is nobody waiting to be served!";
   }
 }
