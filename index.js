c22e126656973c5682a457a0a0977a1c90d3bc04
John Raber <jrab224@gmail.com>
2018-04-15 20:13:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine, name) {
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
 }
 
-function nowServing() {
+function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
       return `Currently serving ${katzDeliLine.shift()}.`;
   } else {
